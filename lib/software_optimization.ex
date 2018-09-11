a7a9481b0e210d044251d2fcae6814b34af7251d
Roman Mingazeev <Direnol@yandex.ru>
2018-09-11 23:14:07 +0700

    init

---
+++ lib/software_optimization.ex
@@ -0,0 +1,18 @@
+defmodule SoftwareOptimization do
+  @moduledoc """
+  Documentation for SoftwareOptimization.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoftwareOptimization.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/software_optimization_test.exs
@@ -0,0 +1,8 @@
+defmodule SoftwareOptimizationTest do
+  use ExUnit.Case
+  doctest SoftwareOptimization
+
+  test "greets the world" do
+    assert SoftwareOptimization.hello() == :world
+  end
+end
